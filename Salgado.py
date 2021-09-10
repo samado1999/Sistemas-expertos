f35e9cdad768d5858c96ae1e85814915b386a7b5
Samuel Amado Ramirez <samuelamadoramirez@gmail.com>
2021-09-10 10:52:29 -0500

    first commit

---
+++ Salgado.py
@@ -0,0 +1,20 @@
+lista = list()
+
+for var in range(15, 999):
+    if var % 5 == 0:
+        lista.append(var)
+    if len(lista) == 40:
+        break
+
+# print(sum(lista))
+
+#2
+for var2 in range(1, 71):
+    # print(7 * var2)
+    print()
+
+#3
+for var3 in range(39, 501):
+    rel = (((2 * var3) ** 0.5) / ((2 * var3) ** 0.5))
+    print(rel)
+
